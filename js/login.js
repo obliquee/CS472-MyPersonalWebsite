1be235ef8d5e75b2b0f07e30e0259c1956629b37
Sameer Adhikari <30802130+obliquee@users.noreply.github.com>
2022-11-09 15:19:13 -0600

    changes in login

--- js/login.js
+++ js/login.js
@@ -1,20 +1,21 @@
 "use strict";
 window.onload = function(){
 
-   function showInputField(){
+   function showInputField(event){
       const userName = document.getElementById("emailAddress").value;
       console.log("UserName : " + userName);
       const userPassword = document.getElementById("userpassword").value;
       console.log("Password : " + userPassword);
       const websiteUrl = document.getElementById("websiteUrl").value;
       console.log("Website Url : " + websiteUrl);
+
+      event.preventDefault();
    
    }
 
-   const submitBtn = document.getElementById("submit");
-   submitBtn.addEventListener("click", showInputField);
+   const submitBtn = document.getElementById("loginForm");
+   submitBtn.addEventListener("submit", showInputField);
 
-   
    
   
 }
